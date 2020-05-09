345308e7d5a1fb84065b9e9906b2b43ec38034e0
unknown <am.vargasg@uniandes.edu.co>
2020-05-08 19:57:15 -0500

    op

--- src/main/java/uniandes/isis2304/alohandes/persistencia/SQLOperador.java
+++ src/main/java/uniandes/isis2304/alohandes/persistencia/SQLOperador.java
@@ -76,7 +76,11 @@ public class SQLOperador {
 	 */
 	public long eliminarOperadorPorId (PersistenceManager pm, long idOperador)
 	{
-        Query q = pm.newQuery(SQL, "DELETE FROM " + pp.darTablaOperador() + " WHERE id = ?");
+		String sql = "SET TRANSACTION ISOLATION LEVEL SERIALIZABLE"; 
+		sql += "BEGIN TRAN";
+		sql += "DELETE FROM " + pp.darTablaOperador() + " WHERE id = ?";
+		sql += "COMMIT TRAN";
+        Query q = pm.newQuery(SQL, sql);
         q.setParameters(idOperador);
         return (long) q.executeUnique();            
 	}
@@ -90,7 +94,11 @@ public class SQLOperador {
 	 */
 	public Operador darOperadorPorId (PersistenceManager pm, long idOperador) 
 	{
-		Query q = pm.newQuery(SQL, "SELECT * FROM " + pp.darTablaOperador() + " WHERE id = ?");
+		String sql = "SET TRANSACTION ISOLATION LEVEL SERIALIZABLE"; 
+		sql += "BEGIN TRAN";
+		sql += "SELECT * FROM " + pp.darTablaOperador() + " WHERE id = ?";
+		sql += "COMMIT TRAN";
+		Query q = pm.newQuery(SQL, sql);
 		q.setResultClass(Operador.class);
 		q.setParameters(idOperador);
 		return (Operador) q.executeUnique();
@@ -104,7 +112,11 @@ public class SQLOperador {
 	 */
 	public long eliminarOperadorPorLogin (PersistenceManager pm, long loginOperador)
 	{
-        Query q = pm.newQuery(SQL, "DELETE FROM " + pp.darTablaOperador() + " WHERE login = ?");
+		String sql = "SET TRANSACTION ISOLATION LEVEL SERIALIZABLE"; 
+		sql += "BEGIN TRAN";
+		sql += "DELETE FROM " + pp.darTablaOperador() + " WHERE login = ?";
+		sql += "COMMIT TRAN";
+        Query q = pm.newQuery(SQL, sql);
         q.setParameters(loginOperador);
         return (long) q.executeUnique();            
 	}
@@ -118,7 +130,11 @@ public class SQLOperador {
 	 */
 	public Operador darOperadorPorLogin (PersistenceManager pm, String loginOperador) 
 	{
-		Query q = pm.newQuery(SQL, "SELECT * FROM " + pp.darTablaOperador() + " WHERE login = ?");
+		String sql = "SET TRANSACTION ISOLATION LEVEL SERIALIZABLE"; 
+		sql += "BEGIN TRAN";
+		sql += "SELECT * FROM " + pp.darTablaOperador() + " WHERE login = ?";
+		sql += "COMMIT TRAN";
+		Query q = pm.newQuery(SQL, sql);
 		q.setResultClass(Operador.class);
 		q.setParameters(loginOperador);
 		return (Operador) q.executeUnique();
@@ -132,7 +148,11 @@ public class SQLOperador {
 	 */
 	public List<Operador> darOperadores(PersistenceManager pm)
 	{
-		Query q = pm.newQuery(SQL, "SELECT * FROM " + pp.darTablaOperador());
+		String sql = "SET TRANSACTION ISOLATION LEVEL SERIALIZABLE"; 
+		sql += "BEGIN TRAN";
+		sql += "SELECT * FROM " + pp.darTablaOperador();
+		sql += "COMMIT TRAN";
+		Query q = pm.newQuery(SQL, sql);
 		q.setResultClass(Operador.class);
 		return (List<Operador>) q.executeList();
 	}
@@ -148,7 +168,9 @@ public class SQLOperador {
 	 */
 	public List<Object> darDineroAnioActual(PersistenceManager pm)
 	{
-		String sql = "with anio as(";
+		String sql = "SET TRANSACTION ISOLATION LEVEL SERIALIZABLE"; 
+		sql += "BEGIN TRAN";
+		sql += "with anio as(";
 		sql += "select EXTRACT(YEAR FROM fecha_fin) AS fecha, res.monto_total AS monto, op.id as id ";
 		sql += " FROM " + pp.darTablaOperador()+"op";
 		sql+= "INNER JOIN" + pp.darTablaHabitacion()+"hab ON(op.habitaciones = hab.id)";
@@ -161,6 +183,8 @@ public class SQLOperador {
 		sql += "FROM anio";
 		sql += "WHERE EXTRACT(YEAR FROM CURRENT_TIMESTAMP) = fecha";
 		sql += "GROUP BY id, fecha";
+		sql += sql += "COMMIT TRAN";
+		
 		Query q = pm.newQuery(SQL, sql); 
 		return q.executeList();
 	}
@@ -173,7 +197,9 @@ public class SQLOperador {
 	 */
 	public List<Object> darDineroAnioCorrido(PersistenceManager pm)
 	{
-		String sql ="select op.id , count( res.monto_total )";
+		String sql = "SET TRANSACTION ISOLATION LEVEL SERIALIZABLE"; 
+		sql += "BEGIN TRAN";
+		sql += "select op.id , count( res.monto_total )";
 		sql += " FROM " + pp.darTablaOperador()+"op";
 		sql+= "INNER JOIN" + pp.darTablaHabitacion()+"hab ON(op.habitaciones = hab.id)";
 		sql+= "INNER JOIN" + pp.darTablaApartamento() +"ap ON(op.apartamentos = ap.id)";
@@ -181,6 +207,7 @@ public class SQLOperador {
 		sql+= "INNER JOIN" + pp.darTablaReservaApartamento()+"resap ON(ap.id = resap.id_Apartamento)";
 		sql+= "INNER JOIN" + pp.darTablaReserva()+"res ON(res.id = resha.id_Reserva AND res.id = respa.id_Reserva)";
 		sql += "WHERE  fecha_fin > DATE_SUB(CURRENT_TIMESTRAP, INTERVAL 1 YEAR)  AND fecha_fin <= CURRENT_TIMESTRAP";
+		sql += sql += "COMMIT TRAN";
 		Query q = pm.newQuery(SQL, sql); 
 		return q.executeList();
 	}
